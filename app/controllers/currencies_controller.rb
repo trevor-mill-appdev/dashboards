36687636e1c3ee681c436796f10a7a78d4b79731
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-05 00:40:03 +0000

    first api works

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -9,4 +9,20 @@ class CurrenciesController < ApplicationController
     render({ :template => "currency_templates/step_one.html.erb"})
   end
   
+  def second_currency
+    @from_currency = params.fetch(:from_currency)
+    raw_data = open("https://api.exchangerate.host/symbols").read
+    parsed_data = JSON.parse(raw_data)
+    symbols_hash = parsed_data.fetch("symbols")
+
+    @array_of_symbols = symbols_hash.keys
+    render({ :template => "currency_templates/step_two.html.erb"})
+  end
+
+  def exchange
+    @from_currency = params.fetch(:from_currency)
+    @to_currency = params.fetch(:to_currency)
+
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
  
   get("/forex", { :controller => "currencies", :action => "first_currency" })
-  
+
+  get("/forex/:from_currency", { :controller => "currencies", :action => "second_currency" })
+
+  get("/forex/:from_currency/:to_currency", { :controller => "currencies", :action => "exchange" })
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
