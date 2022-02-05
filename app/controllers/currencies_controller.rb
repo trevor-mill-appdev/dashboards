e894df009ff5fb2c281296459d98a4a33dd39bd1
trevor-mill <trevor.million@chicagobooth.edu>
2022-02-05 01:04:47 +0000

    it's alive!!!

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -23,6 +23,11 @@ class CurrenciesController < ApplicationController
     @from_currency = params.fetch(:from_currency)
     @to_currency = params.fetch(:to_currency)
 
-    
+    raw_data = open("https://api.exchangerate.host/convert?from=" + @from_currency + "&to=" + @to_currency).read
+    parsed_data = JSON.parse(raw_data)
+    rate_hash = parsed_data.fetch("info")
+    @exchange = rate_hash.fetch("rate")
+
+    render({ :template => "currency_templates/exchange.html.erb"})
   end
 end
